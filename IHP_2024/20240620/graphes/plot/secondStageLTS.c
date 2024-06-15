09da3c7ea9753dd508ec3400a3002069198f7171
kjm_laptop <larc.hk2000.4649@gmail.com>
2024-06-15 17:01:08 +0900

    ''

---
+++ IHP_2024/20240620/graphes/plot/secondStageLTS.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+
+    FILE* pipe = popen("gnuplot -persist", "w");
+    if(pipe == NULL)
+    {
+        fputs("couldnt open pipe.", stderr);
+        return 0;
+    }
+
+    int count = 0;
+
+    //gnuplot setting
+    #pragma region 
+    fputs("set logscale x\n", pipe);
+    //fputs("set logscale y\n", pipe);
+    fputs("set datafile separator \",\" \n", pipe);
+    fputs("set grid xtics mxtics ytics linewidth 2, linewidth 1, linewidth 1\n", pipe);
+    fputs("set tics font \"Arial,20\"\n", pipe);
+    fputs("set xlabel \"frequency [Hz]\" font \"Arial,30\" offset 0,-1.5\n", pipe);
+    fputs("set ylabel \"gain [dB]\" font \"Arial,30\" offset -8,0\n", pipe);
+    fputs("set key font\"Arial,25\"\n", pipe);
+    fputs("set key top right spacing 2.5 offset 0,0\n", pipe);
+    fputs("set terminal windows size 1000,700\n", pipe);
+    fputs("set lmargin 20\n", pipe);
+    fputs("set rmargin 20\n", pipe);
+    fputs("set bmargin 6\n", pipe);
+    fputs("set tmargin 2\n", pipe);
+    fputs("set tics font \"Arial,25\"\n", pipe);
+    fputs("set xtics  offset 0 , -0.8\n", pipe);
+    fputs("set ytics  offset 0 , 0\n", pipe);
+    fputs("set mxtics 10\n", pipe);
+    fputs("set mytics 5\n", pipe);
+    fputs("set grid xtics mxtics ytics linewidth 2, linewidth 1, linewidth 1\n", pipe);
+    fputs("set xrange [1e6 : 1e12]\n", pipe);
+    fputs("set yrange [ : ]\n", pipe);
+    #pragma endregion
+
+    //凡例の設定
+    #pragma region
+    /*
+    fputs("set label 1   center at graph 0.89 , 0.50 \"10 nH\" font \", 20\" \n", pipe);
+    fputs("set label 2   center at graph 0.85 , 0.68 \"15 nH\" font \", 20\" \n", pipe);
+    fputs("set label 3   center at graph 0.83 , 0.80 \"20 nH\" font \", 20\" \n", pipe);
+    fputs("set label 4   center at graph 0.82 , 0.90 \"25 nH\" font \", 20\" \n", pipe);
+    fputs("set label 5   center at graph 0.79 , 0.95 \"30 nH\" font \", 20\" \n", pipe);
+    fputs("set label 6   center at graph 0.53 , 0.12 \"10 nH\" font \", 20\" \n", pipe);
+    fputs("set label 7   center at graph 0.53 , 0.23 \"15 nH\" font \", 20\" \n", pipe);
+    fputs("set label 8   center at graph 0.53 , 0.29 \"20 nH\" font \", 20\" \n", pipe);
+    fputs("set label 9   center at graph 0.82 , 0.90 \"     \" font \", 20\" \n", pipe);
+    fputs("set label 10  center at graph 0.30 , 0.38 \"30 nH\" font \", 20\" \n", pipe);
+    */
+    #pragma endregion
+
+    fputs("LTS = \"..\\\\data\\\\TIASecondStageLTS.txt\"\n", pipe);
+    fputs("virtuoso = \"..\\\\data\\\\secondStage.vcsv\"\n", pipe);
+
+    fprintf(pipe, "plot     LTS skip 1 using 1 : 2 with lines title \"LTS\" \n");
+    fprintf(pipe, "replot   virtuoso skip 6 using 1 : 2  with lines title \"IHP\" \n");
+    
+    pclose(pipe);
+
+    return 0;
+}
