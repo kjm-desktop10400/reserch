4a9297ed3df55fb3ae2685891f15a1511fd9d4b1
kjm_laptop <larc.hk2000.4649@gmail.com>
2024-06-23 17:38:20 +0900

    ''

--- IHP_2024/20240628/graphes/plot/Tx.c
+++ IHP_2024/20240628/graphes/plot/Tx.c
@@ -11,17 +11,23 @@ int main(void)
         return 0;
     }
 
+    //pipe = fopen("plotfile.plt", "w");
+
     int count = 0;
 
     //gnuplot setting
     #pragma region 
     fputs("set logscale x\n", pipe);
-    //fputs("set logscale y\n", pipe);
+    fputs("set logscale y\n", pipe);
+    fputs("set logscale z\n", pipe);
     fputs("set datafile separator \",\" \n", pipe);
-    fputs("set grid xtics mxtics ytics linewidth 2, linewidth 1, linewidth 1\n", pipe);
+    //fputs("set terminal postscript eps enhanced \",\" \n", pipe);
+    //fputs("set output \"output.eps\" \n", pipe);
     fputs("set tics font \"Arial,20\"\n", pipe);
     fputs("set xlabel \"frequency [Hz]\" font \"Arial,30\" offset 0,-1.5\n", pipe);
-    fputs("set ylabel \"Trans-Impedance [k ohm]\" font \"Arial,30\" offset -8,0\n", pipe);
+    //fputs("set format x \"10^{%%L}\" \n", pipe);
+    //fputs("set format y \"10^{%%L}\" \n", pipe);
+    fputs("set ylabel \"Gc [ohm]\" font \"Arial,30\" offset -8,0\n", pipe);
     fputs("set key font\"Arial,25\"\n", pipe);
     fputs("set key top right spacing 2.5 offset 0,0\n", pipe);
     fputs("set terminal windows size 1000,700\n", pipe);
@@ -30,13 +36,13 @@ int main(void)
     fputs("set bmargin 6\n", pipe);
     fputs("set tmargin 2\n", pipe);
     fputs("set tics font \"Arial,25\"\n", pipe);
-    fputs("set xtics  offset 0 , -0.8\n", pipe);
-    fputs("set ytics  offset 0 , 0\n", pipe);
+    //fputs("set xtics  offset 0 , -0.8\n", pipe);
+    //fputs("set ytics  offset 0 , 0\n", pipe);
     fputs("set mxtics 10\n", pipe);
     fputs("set mytics 5\n", pipe);
     fputs("set grid xtics mxtics ytics linewidth 2, linewidth 1, linewidth 1\n", pipe);
     fputs("set xrange [1e6 : 1e12]\n", pipe);
-    fputs("set yrange [ : ]\n", pipe);
+    fputs("set yrange [1 : 1e9]\n", pipe);
     #pragma endregion
 
     //凡例の設定
@@ -67,12 +73,12 @@ int main(void)
     fputs("Cpd = 16e-15  \n", pipe);
     fputs("Cjc = 2.396e-15  \n", pipe);
     fputs("Cje = 12.95e-15  \n", pipe);
-    fputs("f(x) = (gm + gce) / ( Ge*gce + Gc*( Ge + gm + gie + gce ) + i * 2*pi* x * ( gce*(Cpd+Cje) + (Ge + gm + gie + gce) * Cjc + Gc * (Cpd+Cje) ) - 4 * pi * pi * Cjc (Cpd + Cje) ) \n", pipe);
-    
+    fputs("f(x) = (gm + gce) / ( Ge*gce + Gc*( Ge + gm + gie + gce ) + i * 2*pi* x * ( gce*(Cpd+Cje) + (Ge + gm + gie + gce) * Cjc + Gc * (Cpd+Cje) ) - 4 * pi * pi * Cjc * (Cpd + Cje) ) \n", pipe);
+    fputs("g(x, y) = (gm + gce) / ( Ge*gce +y*( Ge + gm + gie + gce ) + i * 2*pi* x * ( gce*(Cpd+Cje) + (Ge + gm + gie + gce) * Cjc + y * (Cpd+Cje) ) - 4 * pi * pi * Cjc * (Cpd + Cje) ) \n", pipe);
+    fputs("p(x, y) = 5e3+x*1e-36 \n", pipe);
 
 
-    fprintf(pipe, "plot f(x) with lines title \"\" \n");
-    
+    fprintf(pipe, "splot abs(g(x, 1/y)), p(x, y) with lines title \"X_{T}\" \n");    
 
     pclose(pipe);
 
